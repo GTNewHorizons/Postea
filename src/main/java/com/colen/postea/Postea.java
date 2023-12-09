cf0743435ed60e74214eb5caf55092117afbe686
Martin Robertz <dream-master@gmx.net>
2023-12-09 17:08:47 +0100

    update gradle update deps sa remove not use action scripts

--- src/main/java/com/colen/postea/Postea.java
+++ src/main/java/com/colen/postea/Postea.java
@@ -1,15 +1,17 @@
 package com.colen.postea;
 
+import java.util.function.Function;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+
 import com.colen.postea.API.BlockReplacementManager;
 import com.colen.postea.Utility.BlockConversionInfo;
+
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-
-import java.util.function.Function;
 
 @Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
 @SuppressWarnings("unused")
@@ -18,7 +20,8 @@ public class Postea {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 
-        ModContainer activeMod = Loader.instance().activeModContainer();
+        ModContainer activeMod = Loader.instance()
+            .activeModContainer();
         String modID = activeMod.getModId();
         String v = activeMod.getVersion();
 
@@ -26,13 +29,15 @@ public class Postea {
 
             BlockConversionInfo blockConversionInfoNew = new BlockConversionInfo();
 
-            if ((blockConversionInfoOld.x == -1) && (blockConversionInfoOld.y == 3) && (blockConversionInfoOld.z == -1)) {
+            if ((blockConversionInfoOld.x == -1) && (blockConversionInfoOld.y == 3)
+                && (blockConversionInfoOld.z == -1)) {
                 blockConversionInfoNew.blockID = Block.getIdFromBlock(Blocks.wool);
                 blockConversionInfoNew.metadata = 1;
                 return blockConversionInfoNew;
             }
 
-            if ((blockConversionInfoOld.x == -1) && (blockConversionInfoOld.y == 3) && (blockConversionInfoOld.z == 1)) {
+            if ((blockConversionInfoOld.x == -1) && (blockConversionInfoOld.y == 3)
+                && (blockConversionInfoOld.z == 1)) {
                 blockConversionInfoNew.blockID = Block.getIdFromBlock(Blocks.wool);
                 blockConversionInfoNew.metadata = 1;
                 return blockConversionInfoNew;
@@ -44,7 +49,8 @@ public class Postea {
                 return blockConversionInfoNew;
             }
 
-            if ((blockConversionInfoOld.x == 1) && (blockConversionInfoOld.y == 3) && (blockConversionInfoOld.z == -1)) {
+            if ((blockConversionInfoOld.x == 1) && (blockConversionInfoOld.y == 3)
+                && (blockConversionInfoOld.z == -1)) {
                 blockConversionInfoNew.blockID = Block.getIdFromBlock(Blocks.wool);
                 blockConversionInfoNew.metadata = 1;
                 return blockConversionInfoNew;
