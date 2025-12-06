ed3ba1889d591e6f76a3e0651c0ea15ae84f2a0e
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-12-06 17:30:41 +0200

    documentation

--- src/main/java/com/gtnewhorizons/postea/api/TileEntityReplacementManager.java
+++ src/main/java/com/gtnewhorizons/postea/api/TileEntityReplacementManager.java
@@ -14,12 +14,25 @@ public class TileEntityReplacementManager {
 
     private static final HashMap<String, TriFunction<NBTTagCompound, World, Chunk, BlockInfo>> tileEntityToNormalBlockTransformer = new HashMap<>();
 
+    /**
+     * @deprecated Superseded by {@link #tileEntityTransformer(String, TriFunction)}.
+     */
+    @Deprecated
     public static void tileEntityTransformer(String tileEntityId,
         BiFunction<NBTTagCompound, World, BlockInfo> transformerFunction) {
         tileEntityToNormalBlockTransformer
             .put(tileEntityId, (tag, world, chunk) -> transformerFunction.apply(tag, world));
     }
 
+    /**
+     * Register a callback function to transform a tile entity before it's loaded.
+     * Callback returns a {@link BlockInfo} which the tile entity will be transformed into.</br>
+     * Note that the callback will be called before the world is fully loaded, and world.getBlock will throw an error.
+     * Use chunk.getBlock(x & 15, y, z & 15) instead.
+     *
+     * @param tileEntityId        ID of the tile entity to be transformed
+     * @param transformerFunction Callback function to transforms the tile entity
+     */
     public static void tileEntityTransformer(String tileEntityId,
         TriFunction<NBTTagCompound, World, Chunk, BlockInfo> transformerFunction) {
         tileEntityToNormalBlockTransformer.put(tileEntityId, transformerFunction);
